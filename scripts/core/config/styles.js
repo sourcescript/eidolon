bf5e159e1f2327fc708ae36b8fa2bac2d5abd943
srph <seraphipod@gmail.com>
2015-04-01 02:19:04 +0800

    Refactored style variables.
    - Updated base colors.
    - Added some bases.

--- scripts/core/config/styles.js
+++ scripts/core/config/styles.js
@@ -1,11 +1,28 @@
 /**
  * Contains all stylus variables
  */
+var _ = {};
 
-module.exports = {
-  'color-base': 'red',
-  'color-primary': 'blue',
-  'color-success': 'green',
-  'color-warning': 'yellow',
-  'color-danger': 'red'
-};
+_['color-primary']  = '#4AB1D1';
+_['color-success']  = '#4AD1A3';
+_['color-warning']  = '#E5E851';
+_['color-danger']   = '#F24A53';
+_['color-blocked']  = '#4D5159';
+_['color-grey']     = 'rgba(255, 255, 255, 0.7)';
+
+_['color-base']       = '#18222B';
+_['color-highlight']  = '#F24A53';
+
+/**
+ * Typography
+ */
+ _['base-line-height'] = 1.428;
+ _['base-font-size'] = 14;
+_['h1'] = 36;
+_['h2'] = 30;
+_['h3'] = 24;
+_['h4'] = 18;
+_['h5'] = 14;
+_['h6'] = 12;
+
+module.exports = _;
