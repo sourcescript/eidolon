1345d2ce1fcc8586ffc23c9c99cfe9f6c280e032
srph <seraphipod@gmail.com>
2015-04-02 15:29:13 +0800

    Init Buttons.

---
+++ scripts/template/Button/index.js
@@ -0,0 +1,7 @@
+exports.Default = require('./Default');
+exports.Primary = require('./Primary');
+exports.Warning = require('./Warning');
+exports.Success = require('./Success');
+exports.Blocked = require('./Blocked');
+exports.Danger = require('./Danger');
+exports.Inverse = require('./Inverse/index');
